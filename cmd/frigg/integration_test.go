1555b835f28d926896f8fca7475c1c25c4cff993
Lasse Hels <lasse.hels@maersk.com>
2025-06-07 15:27:52 +0200

    add tests

---
+++ cmd/frigg/integration_test.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
+)
+
+func TestFriggIntegration(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping integration test in short mode")
+	}
+
+	var out bytes.Buffer
+
+	ctx, cancel := context.WithCancel(t.Context())
+	// Schedule a cancellation to ensure that the context is cancelled even if the test fails before the context is
+	// cancelled manually.
+	defer cancel()
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		return run(ctx, &out)
+	})
+
+	serverURL := "http://localhost:8080"
+	waitForServer(t, serverURL)
+
+	t.Run("health endpoint returns healthy", func(t *testing.T) {
+		body := assertOK(t, fmt.Sprintf("%s/health", serverURL))
+		assert.Equal(t, "healthy", body, "Health endpoint returned incorrect body")
+	})
+
+	t.Run("metrics endpoint returns metrics", func(t *testing.T) {
+		body := assertOK(t, fmt.Sprintf("%s/metrics", serverURL))
+		assert.Contains(t, body, "go_", "Metrics should contain Go metrics")
+		assert.Contains(t, body, "process_", "Metrics should contain process metrics")
+	})
+
+	logs := out.String()
+	assert.Contains(t, logs, `"msg":"Registered route","path":"/health","methods":["GET"]`)
+	assert.Contains(t, logs, `"msg":"Registered route","path":"/metrics","methods":["GET"]`)
+
+	t.Run("shuts down gracefully", func(t *testing.T) {
+		cancel()
+		err := eg.Wait()
+		require.NoError(t, err)
+	})
+}
+
+func waitForServer(t *testing.T, url string) {
+	require.Eventually(t, func() bool {
+		resp, err := http.Get(url)
+		if err != nil {
+			return false
+		}
+
+		_ = resp.Body.Close()
+		return true
+	}, 5*time.Second, 10*time.Millisecond, "Server did not start in time")
+}
+
+func assertOK(t *testing.T, url string) string {
+	resp, err := http.Get(url)
+	require.NoError(t, err, "Failed to make request to URL %q", url)
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode, "URL %q returned incorrect status code", url)
+
+	body, err := io.ReadAll(resp.Body)
+	require.NoError(t, err, "Failed to read response body")
+	return string(body)
+}
