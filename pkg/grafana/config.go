0856ced7451dbf3c048aec080babacaed2b8a049
Lasse Hels <lasse.hels@maersk.com>
2025-10-11 14:51:45 +0200

    Remove redundant comment from grafana.Secrets
    The Tokens field documentation is already comprehensive in the README,
    so we don't need to duplicate it in the code.

--- pkg/grafana/config.go
+++ pkg/grafana/config.go
@@ -16,17 +16,5 @@ type PruneConfig struct {
 }
 
 type Secrets struct {
-	// Tokens used to authenticate with Grafana's API for specific namespaces. This field is a map where keys are
-	// namespace names and values are the token used to authenticate with Grafana's API for that namespace. A namespace's
-	// token is expected to have permissions to list and delete dashboards in that namespace.
-	//
-	// This field also controls which namespaces Frigg will prune and which it will ignore; Frigg will only prune
-	// namespaces that have an entry in this map.
-	//
-	// See https://grafana.com/docs/grafana/v12.0/developers/http_api/apis/#namespace-namespace.
-	//
-	// The map must contain at least one namespace.
-	//
-	// Required.
 	Tokens map[string]string `yaml:"tokens" validate:"required,min=1,dive,keys,required,endkeys,required"`
 }
